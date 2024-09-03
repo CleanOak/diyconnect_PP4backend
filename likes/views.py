6a440146e864221fe0033e6127d891ed39f90d7f
Morgan Asare <ohenemorgan@gmail.com>
2024-09-03 08:38:26 +0000

    add like app, model and serializer

---
+++ likes/views.py
@@ -0,0 +1,22 @@
+from rest_framework import generics, permissions
+from diy_connect.permissions import IsOwnerReadOnly
+from .models import Like
+from .serializers import LikeSerializer
+
+
+class LikeList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    permission_classes = [IsOwnerReadOnly]
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
+
+
+
