501f5349fde3ca84fb7839c8e27a764d66fa772d
Morgan Asare <ohenemorgan@gmail.com>
2024-09-15 20:12:21 +0000

    change media_url back to media

--- profiles/models.py
+++ profiles/models.py
@@ -9,7 +9,7 @@ class Profile(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
-    image = CloudinaryField('image', default='no_image_mvrwpr')
+    image = models.ImageField(upload_to='images/', default='no_image_mvrwpr')
 
     
    
