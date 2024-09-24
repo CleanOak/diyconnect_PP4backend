8f71f1ab513fedfddd1ef9766659083dd4fc610e
Morgan Asare <ohenemorgan@gmail.com>
2024-09-24 18:59:51 +0000

    add updated JWT configuration

--- diy_connect/settings.py
+++ diy_connect/settings.py
@@ -44,14 +44,23 @@ if 'DEV' not in os.environ:
         'rest_framework.renderers.JSONRenderer',
     ]
 
-REST_USE_JWT = True
-JWT_AUTH_SECURE = True
-JWT_AUTH_COOKIE = "my-app-auth"
-JWT_AUTH_REFRESH_COOKIE = "app-refresh-token"
-JWT_AUTH_SAMESITE = 'None'
+# REST_USE_JWT = True
+# JWT_AUTH_SECURE = True
+# JWT_AUTH_COOKIE = "my-app-auth"
+# JWT_AUTH_REFRESH_COOKIE = "app-refresh-token"
+# JWT_AUTH_SAMESITE = 'None'
 
-REST_AUTH_SERIALIZERS = {
-    'USER_DETAILS_SERIALIZER': 'diy_connect.serializers.CurrentUserSerializer'
+# REST_AUTH_SERIALIZERS = {
+#     'USER_DETAILS_SERIALIZER': 'diy_connect.serializers.CurrentUserSerializer'
+# }
+
+REST_AUTH = {
+    'USE_JWT': True,
+    'JWT_AUTH_SECURE': True,
+    'JWT_AUTH_COOKIE': 'my-app-auth',
+    'JWT_AUTH_REFRESH_COOKIE': 'my-refresh-token',
+    'JWT_AUTH_SAMESITE': 'None',
+    'USER_DETAILS_SERIALIZER': 'diy_connect.serializers.CurrentUserSerializer',
 }
 
 
