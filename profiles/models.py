d8d999be8e9dd570336d3c6a29ca6ca6891feb5c
Morgan Asare <ohenemorgan@gmail.com>
2024-09-29 14:15:18 +0000

    add imagefield to profiles model

--- profiles/models.py
+++ profiles/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
-from cloudinary.models import CloudinaryField
+
 
 class Profile(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -9,7 +9,7 @@ class Profile(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
-    image = CloudinaryField('images', default='nobody_nruqan')
+    image = models.ImageField(upload_to='images/', default='../no_image_mvrwpr', blank=True)
 
     
    
