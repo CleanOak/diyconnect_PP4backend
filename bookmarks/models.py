ca8e2b330296b62242078a2255a7787ef9f45780
Morgan Asare <ohenemorgan@gmail.com>
2024-11-18 18:04:03 +0000

    add serializers to notifications and bookemarks

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from posts.models import Post
 
-# Create your models here.
+
+class Bookmark(models.Model):
+    """
+    Bookmark model relating to post
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bookmarks")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="bookmarked_by")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        """
+        Class to order in descending order
+        """
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.user} bookmarked {self.post}"
