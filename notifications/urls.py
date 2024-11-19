e3160c618069a4317bed2dab5ab6301de40dd062
Morgan Asare <ohenemorgan@gmail.com>
2024-11-19 20:41:33 +0000

    amend urls for notifications

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import NotificationList, NotificationDetail, UnreadNotificationCount
+from notifications import views
 
 urlpatterns = [
-    path('notifications/', NotificationList.as_view(), name='notification-list'),
-    path('notifications/<int:id>/', NotificationDetail.as_view(), name='notification-detail'),
-    path('notifications/unread-count/', UnreadNotificationCount.as_view(), name='unread-count'),
+    path('notifications/', views.NotificationList.as_view(), name='notification-list'),
+    path('notifications/<int:id>/', views.NotificationDetail.as_view(), name='notification-detail'),
 ]
